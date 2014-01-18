bff871ba6b308dc6927eb832cd9635221808c2d0
Fred Cox <fred@ekreative.com>
2014-01-18 15:09:47 +0200

    Create a basic example for appshed usage in symfony

---
+++ src/AppShed/Extensions/RedmineBundle/AppShedExtensionsRedmineBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AppShed\Extensions\RedmineBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AppShedExtensionsRedmineBundle extends Bundle
+{
+}
---
+++ src/AppShed/Extensions/RedmineBundle/Controller/DefaultController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace AppShed\Extensions\RedmineBundle\Controller;
+
+use AppShed\Element\Item\Button;
+use AppShed\Element\Item\Input;
+use AppShed\Element\Item\Text;
+use AppShed\Element\Screen\Screen;
+use AppShed\HTML\Remote;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function indexAction()
+    {
+        $screen = new Screen("Hello Word");
+
+        $screen->addChild(new Text("Hi!"));
+
+        $inp = new Input("name", "What is your name?");
+        $screen->addChild($inp);
+
+        $btn = new Button("Go");
+        $btn->setRemoteLink($this->generateUrl("hello", [], true));
+        $btn->addVariable($inp);
+        $screen->addChild($btn);
+
+        $remote = new Remote($screen);
+        return $remote->getSymfonyResponse();
+    }
+
+    /**
+     * @Route("/hello", name="hello")
+     */
+    public function helloAction(Request $request)
+    {
+        $screen = new Screen("Hello Word");
+
+        $name = $request->query->get('name');
+        $screen->addChild(new Text("Hi $name!"));
+
+        $remote = new Remote($screen);
+        return $remote->getSymfonyResponse();
+    }
+}
